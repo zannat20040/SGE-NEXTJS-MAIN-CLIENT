f85053274a6fa66a0884e12e95100e0f55d42539
zannat20040 <shanzidadisha1234@gmail.com>
2024-12-27 03:10:05 +0600

    university check dynamic

--- src/_components/shared/SingleUniversity/SingleUniversity.jsx
+++ src/_components/shared/SingleUniversity/SingleUniversity.jsx
@@ -3,9 +3,9 @@ import { useState, useEffect } from "react";
 import Banner1 from "./Single_University_Banner";
 import data from "../../../assets/json/ukUni.json";
 import Single_Universty_Accordion from "./Single_Universty_Accordion";
-import Single_University_Check from "./Single_University_Check";
 import Single_University_Info from "./Single_University_Info";
 import Single_University_Form from "./Single_University_Form";
+import Single_University_Check from "./Single_University_Check";
 
 const SingleUniversity = ({ universityDetails }) => {
   console.log("University Details 11:", universityDetails);
@@ -28,7 +28,7 @@ const SingleUniversity = ({ universityDetails }) => {
           <div className="md:grid md:grid-cols-12 grid-cols-1 mt-8 md:mt-12 gap-10 px-5">
             <div className="lg:col-span-7 col-span-6">
               <Single_Universty_Accordion university={universityDetails} />
-              <Single_University_Check />
+              <Single_University_Check uniName={universityDetails?.name} />
             </div>
             <div className="lg:col-span-5 col-span-6">
               <Single_University_Info university={universityDetails} />
